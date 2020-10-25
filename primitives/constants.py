51c4deff60c4e3c6506d54963a765f6224cbb1ab
Jungtaek Kim <jtkim@postech.ac.kr>
2020-10-25 17:24:06 +0900

    INIT

---
+++ primitives/constants.py
@@ -0,0 +1,3 @@
+PATH_UNIT_MESHES = './unit_meshes'
+STR_CUBE = 'cube.stl'
+STR_LEGO_2_4 = 'lego_2_4.stl'
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+import os
+import sys
+
+
+path_requirements = 'requirements.txt'
+list_packages = ['primitives']
+
+with open(path_requirements) as f:
+    required = f.read().splitlines()
+
+setup(
+    name='primitives',
+    version='0.1.0',
+    author='Jungtaek Kim',
+    author_email='jtkim@postech.ac.kr',
+    url='https://github.com/POSTECH-CVLab/3d-primitives',
+    license='MIT',
+    description='It is for constructing a 3D shape.',
+    packages=list_packages,
+    python_requires='>=3.5, <4',
+    install_requires=required,
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ]
+)
